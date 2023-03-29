9c2de7b2203237748e24760512198ceb424ec4ca
MarcondesJr <marcondestecinfo@gmail.com>
2023-03-29 15:30:32 -0400

    Finalizando aula 02 - módulo 07

---
+++ 07-recursos-e-componentes/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'marcondesjr/fullstackphp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f3f0d504caf00be6d214a23b513a3ffd24df2184',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'marcondesjr/fullstackphp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f3f0d504caf00be6d214a23b513a3ffd24df2184',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
