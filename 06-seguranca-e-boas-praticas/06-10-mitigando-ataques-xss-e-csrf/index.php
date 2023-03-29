e6e0ae1735d3227d404ea2eb182390058e0d8904
MarcondesJr <marcondestecinfo@gmail.com>
2023-03-29 10:43:47 -0400

    Finalizando aula 10 - módulo 06

--- 06-seguranca-e-boas-praticas/06-10-mitigando-ataques-xss-e-csrf/index.php
+++ 06-seguranca-e-boas-praticas/06-10-mitigando-ataques-xss-e-csrf/index.php
@@ -3,19 +3,31 @@ require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("06.10 - Mitigando ataques XSS e CSRF");
 
 require __DIR__ . "/../source/autoload.php";
-
+session_start();
 /*
  * [ XSS ] Cross-site Scripting
  * https://pt.wikipedia.org/wiki/Cross-site_scripting
  */
 fullStackPHPClassSession("xxs", __LINE__);
+//$post = $_POST;
+$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+if ($post){
+    $data = (object)$post;
+    var_dump($post);
 
+    echo $data->first_name;
+}
 
 /*
  * [ CSRF ] Cross-Site Request Forgery
  * https://pt.wikipedia.org/wiki/Cross-site_request_forgery
  */
 fullStackPHPClassSession("csrf", __LINE__);
+if ($_REQUEST && !csrf_verify($_REQUEST)){
+    var_dump("CSRF Bloqueado!");
+}else{
+    var_dump($_REQUEST);
+}
 
 
 /*
